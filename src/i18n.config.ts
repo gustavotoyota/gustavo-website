b049785beec49825941ddd5f6b07a680d5ff22e8
Gustavo Toyota <gustavottoyota@gmail.com>
2024-01-28 21:10:48 -0400

    feat: add i18n

---
+++ src/i18n.config.ts
@@ -0,0 +1,7 @@
+const i18nConfig = {
+  locales: ["en", "pt"],
+
+  defaultLocale: "en",
+};
+
+export default i18nConfig;
---
+++ src/i18n.ts
@@ -0,0 +1,12 @@
+import { notFound } from "next/navigation";
+import { getRequestConfig } from "next-intl/server";
+import i18nConfig from "./i18n.config";
+
+export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming `locale` parameter is valid
+  if (!i18nConfig.locales.includes(locale as any)) notFound();
+
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default,
+  };
+});
